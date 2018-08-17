2f9c69bed11c4caf43ef53ca8d1d928de383161f
Jose Yvimas <joseyvimas@MacBook-Pro-de-Jose.local>
2018-08-17 15:58:16 -0400

    trabajando en login y profile

--- src/app/#interfaces/student.interface.ts
+++ src/app/#interfaces/student.interface.ts
@@ -2,10 +2,10 @@ export interface Student{
     id: string;
     name:string;
     lastname:string;
-    email:string;
     status:string;
-    representative:string;
     address:string;
     long:string;
     lat:string;
+    alt:string;
+    photo:string;
 }
--- src/app/@pages/profile/profile.page.ts
+++ src/app/@pages/profile/profile.page.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService, StudentsService } from "../../@services";
+import { User, Student } from "../../#interfaces";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: 'profile.page.html',
-  styleUrls: ['profile.page.scss'],
+    selector: 'app-profile',
+    templateUrl: 'profile.page.html',
+    styleUrls: ['profile.page.scss'],
 })
 export class ProfilePage {
-    public userType:string = "representative";
-    
-    constructor(){
-        console.log("holña");
+    public userType: string = "representative";
+    public user = {} as User;
+    public student = {} as Student;
+
+    constructor(private authService: AuthService, private router: Router, private studentService: StudentsService) {}
+
+    ngOnInit() {
+        var currentUser = this.authService.getCurrentUser();
+        if (currentUser != null) {
+            this.authService.getUserData(currentUser.uid).then((data) => {
+                this.user = data.payload.data() as User;
+
+                this.studentService.getStudent(this.user.student).then((data) => {
+                    this.student = data.payload.data() as Student;
+                });
+            });
+        } else this.router.navigate(['']);
     }
-
-
 }
