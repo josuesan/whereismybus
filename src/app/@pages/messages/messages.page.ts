75a17fe7f332eaa5f6d45fa52b9a508a19504134
Jose Yvimas <joseyvimas@MacBook-Pro-de-Jose.local>
2018-08-20 18:35:25 -0400

    mostrando el mapa dle inicio solo a los representantes

--- src/app/@pages/messages/messages.page.ts
+++ src/app/@pages/messages/messages.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CTAService, AuthService } from '../../@services';
-import { User } from "../../#interfaces";
 
 @Component({
   selector: 'app-messages',
@@ -12,14 +11,14 @@ export class MessagesPage {
   constructor(private cta: CTAService, private authService: AuthService) { }
   
   ngOnInit(){
-    this.verifyRole;
+    this.verifyRole();
   }
 
   async verifyRole(){
     let currentUser = await this.authService.getCurrentUser();
     if (currentUser != null) {
       this.authService.getUserData(currentUser.uid).then((doc) => {
-        if (doc.exists) this.userType = (doc.data() as User).role;
+        if (doc.exists) this.userType = doc.data().role;
         else this.cta.goToHome();
       }).catch((err) => console.error(err));
     }
