a79c7c4278a1e226a7ff17cb4ec7a8c4e4e8173a
juggers <josueguitar2@gmail.com>
2018-10-08 16:18:01 -0400

    lc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private keyboard: Keyboard
   ) {
     this.initializeApp();
   }
@@ -21,6 +23,11 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.keyboard.setResizeMode("sync");
+      this.keyboard.onKeyboardShow().subscribe((e)=>{
+        console.log(e);
+      })
     });
+    
   }
 }
